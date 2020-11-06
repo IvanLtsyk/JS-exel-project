bcf14f02acb2bc2e4f0f49f454912784c6279dbe
Ivan Lutsyk <ivanlutsyk87@gmail.com>
2020-11-06 18:54:18 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+﻿import "./scss/index.scss";
+
+/*babel test */
+
+/*async function test1() {
+  return await Promise.resolve("Async working!!! 1")
+}*/
+
+async function test2() {
+  return await Promise.resolve("Async working!! 2");
+}
+
+test2().then(console.log);
+console.log("Script working!");
